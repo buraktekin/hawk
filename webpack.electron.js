3557a5e58bf7a6ccd260a13ac351e7b1be389685
buraktekin <tknbrk@gmail.com>
2022-01-03 02:01:07 +0100

    fix configurations for electron and react

--- webpack.electron.js
+++ webpack.electron.js
@@ -12,11 +12,21 @@ module.exports = {
     extensions: ['.tsx', '.ts', '.js'],
   },
   module: {
-    rules: [{
-      test: /\.ts$/,
-      include: /src/,
-      use: [{ loader: 'ts-loader' }]
-    }]
+    rules: [
+      {
+        test: /\.ts$/,
+        include: /src/,
+        use: [{ loader: 'ts-loader' }]
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      }
+    ]
   },
   output: {
     path: __dirname + '/dist',
